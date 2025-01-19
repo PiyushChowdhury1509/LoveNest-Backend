583887360eeb31bdbaa3f9e385aaf5e8be6509d3
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2025-01-20 02:20:09 +0530

    learnt the pre method on mongoose schema

--- src/model/connection.ts
+++ src/model/connection.ts
@@ -1,28 +1,40 @@
-import mongoose,{ Document, Types } from 'mongoose';
+import mongoose, { Document, Types } from "mongoose";
 
-interface connectionSchemaType extends Document{
-    fromUserId: Types.ObjectId,
-    toUserId: Types.ObjectId,
-    status: string
+interface connectionSchemaType extends Document {
+  fromUserId: Types.ObjectId;
+  toUserId: Types.ObjectId;
+  status: string;
 }
 
-const connectionSchema = new mongoose.Schema<connectionSchemaType>({
+const connectionSchema = new mongoose.Schema<connectionSchemaType>(
+  {
     fromUserId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
     toUserId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
     status: {
-        type: String,
-        enum: {
-            values: ["interested","ignored","accepted","rejected"],
-            message: `{VALUE} is incorrect status type`,
-        },
-        required: true
-    }
-}, { timestamps: true });
+      type: String,
+      enum: {
+        values: ["interested", "ignored", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export const Connection = mongoose.model('Connection',connectionSchema);
+connectionSchema.pre('save', function(next){
+    const newConnection = this;
+    if(newConnection.fromUserId.toString()===newConnection.toUserId.toString()){
+        const error = new Error('invalid request, cant send connection to oneself');
+        return next(error);
+    }
+    next()
+})
+
+export const Connection = mongoose.model("Connection", connectionSchema);
--- src/routes/connectionRouter.ts
+++ src/routes/connectionRouter.ts
@@ -1,81 +1,87 @@
-import express,{ Router,Request,Response } from "express";
+import express, { Router, Request, Response } from "express";
 import userAuth from "../middleware/userAuth";
 import { Connection } from "../model/connection";
 import { connectionSchema, connectionTypeWithId } from "../schema/connection";
 import { z } from "zod";
 import { User } from "../model/user";
 import { userTypeWithId } from "../schema/user";
+import { isValidObjectId } from "mongoose";
 
 const connectionRouter: Router = express.Router();
 
-connectionRouter.post('/send/:status/:userId',userAuth, async (req: Request,res: Response)=>{
-    try{
-        const user = (req as any).user;
-        const { status, userId } = req.params;
-        if(!status || !userId){
-            res.status(400).json({
-                message: "request is invalid"
-            })
-            return;
-        }
+connectionRouter.post(
+  "/send/:status/:userId",
+  userAuth,
+  async (req: Request, res: Response) => {
+    try {
+      const user:userTypeWithId = (req as any).user;
+      const { status, userId } = req.params;
+      if (!status || !userId) {
+        res.status(400).json({
+          message: "request is invalid",
+        });
+        return;
+      }
 
-        if (!/^[a-f\d]{24}$/i.test(userId)) {
-            res.status(400).json({ message: "Invalid userId format" });
-            return;
-        }
-        
-        if(!(['interested','ignored'].includes(status))){
-            res.status(400).json({
-                message: "invalid status"
-            })
-            return;
-        }
-        const oppositeUser: userTypeWithId | null = await User.findById(userId);
-        if(!oppositeUser){
-            res.status(400).json({
-                message: "opposite user doesnt exist"
-            })
-            return;
-        }
-        const connection = {
-            fromUserId: user._id,
-            toUserId: userId,
-            status
-        }
-        const validatedConnection = connectionSchema.parse(connection);
-        const existingRequest: connectionTypeWithId | null = await Connection.findOne({
-            $or:[
-                {fromUserId: user._id ,toUserId: userId},
-                {fromUserId: userId, toUserId: user._id}
-            ]
-        }) 
-        if(existingRequest){
-            res.status(400).json({
-                message: "connection request already exist",
-                data: existingRequest
-            })
-            return;
-        }
-        const newConnection = new Connection(validatedConnection);
-        const data = await newConnection.save();
-        res.status(201).json({
-            message: "connection sent successfully",
-            data: data
-        })
+      if (!isValidObjectId(userId)) {
+        res.status(400).json({ message: "Invalid userId format" });
         return;
-    } catch(err){
-        if(err instanceof z.ZodError){
-            res.status(400).json({
-                message: "invalid request data",
-                eror: err.errors
-            })
-            return;
-        }
-        res.status(500).json({
-            message: "something went wrong"
-        })
+      }
+
+      if (!["interested", "ignored"].includes(status)) {
+        res.status(400).json({
+          message: "invalid status",
+        });
         return;
+      }
+      const oppositeUser: userTypeWithId | null = await User.findById(userId);
+      if (!oppositeUser) {
+        res.status(400).json({
+          message: "opposite user doesnt exist",
+        });
+        return;
+      }
+      const connection = {
+        fromUserId: user._id,
+        toUserId: userId,
+        status,
+      };
+      const validatedConnection = connectionSchema.parse(connection);
+      const existingRequest: connectionTypeWithId | null =
+        await Connection.findOne({
+          $or: [
+            { fromUserId: user._id, toUserId: userId },
+            { fromUserId: userId, toUserId: user._id },
+          ],
+        });
+      if (existingRequest) {
+        res.status(400).json({
+          message: "connection request already exist",
+          data: existingRequest,
+        });
+        return;
+      }
+      const newConnection = new Connection(validatedConnection);
+      const data = await newConnection.save();
+      res.status(201).json({
+        message: "connection sent successfully",
+        data: data,
+      });
+      return;
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        res.status(400).json({
+          message: "invalid request data",
+          eror: err.errors,
+        });
+        return;
+      }
+      res.status(500).json({
+        message: "something went wrong",
+      });
+      return;
     }
-})
+  }
+);
 
 export default connectionRouter;
