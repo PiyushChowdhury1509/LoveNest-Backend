6c84e53921effa9f8d47c66a4976b1d63ec9ea0e
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2025-02-03 18:38:06 +0530

    added recieved connections api

--- src/model/connection.ts
+++ src/model/connection.ts
@@ -1,10 +1,44 @@
-import mongoose from 'mongoose';
+import mongoose, { Document, Types } from "mongoose";
 
-const connectionSchema = new mongoose.Schema({
-  fromUserId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  toUserId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  status: { type: String, enum: ['interested', 'ignored', 'accepted', 'rejected'], required: true }
-}, { timestamps: true });
+interface connectionSchemaType extends Document {
+  fromUserId: Types.ObjectId;
+  toUserId: Types.ObjectId;
+  status: string;
+}
 
-export const Connection = mongoose.model('Connection', connectionSchema);
+const connectionSchema = new mongoose.Schema<connectionSchemaType>(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["interested", "ignored", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
+connectionSchema.index({fromUserId: 1,toUserId: 1});
+
+connectionSchema.pre('save', function(next){
+    const newConnection = this;
+    if(newConnection.fromUserId.toString()===newConnection.toUserId.toString()){
+        const error = new Error('invalid request, cant send connection to oneself');
+        return next(error);
+    }
+    next()
+})
+
+export const Connection = mongoose.model("Connection", connectionSchema);
--- src/routes/profileRouter.ts
+++ src/routes/profileRouter.ts
@@ -1,10 +1,11 @@
 import express, { Router, RequestHandler } from "express";
-import { editUser,feedApi } from "../controllers/profile.controller";
+import { editUser,feedApi,recievedConnections } from "../controllers/profile.controller";
 import userAuth from "../middleware/userAuth";
 
 const profileRouter: Router = express.Router();
 
 profileRouter.patch('/editProfile',userAuth, editUser as RequestHandler);
 profileRouter.get('/feed', userAuth, feedApi as RequestHandler);
+profileRouter.get('/recievedConnectionRequests',userAuth,recievedConnections as RequestHandler);
 
 export default profileRouter;
